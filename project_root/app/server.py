707f96572b1be84e8b13c9fed3e85c2e6574169c
R0gfall <kornilovleo@ya.ru>
2025-09-02 12:37:09 +0300

    Start commit nginx & flask

---
+++ project_root/app/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.get("/health")
+def health():
+    return "OK", 200
+
+@app.post("/api")
+def api():
+    data = request.get_json(silent=True) or {}
+    return jsonify(received=data, message="pong"), 200
+
+if __name__ == "__main__":
+    # Слушаем на всех интерфейсах, чтобы эмулятор/устройство видели сервер
+    app.run(host="0.0.0.0", port=9999, debug=True)
